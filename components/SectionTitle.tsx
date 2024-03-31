8c6bf9eab793a47f13cd43dec779d1b4aa0a8703
Olubodun <bolowys33@gmail.com>
2024-03-31 21:59:23 +0100

    Implement section title component

---
+++ components/SectionTitle.tsx
@@ -0,0 +1,13 @@
+import { FC } from "react";
+
+interface SectionTitleProps {
+    section: string,
+}
+
+const SectionTitle: FC<SectionTitleProps> = ({section }) => {
+    return ( <div className="py-2">
+        <span className="tracking-[2px] mb-3 uppercase text-[#222261] text-[13px] font-extrabold">about us</span>
+    </div> );
+}
+ 
+export default SectionTitle;
