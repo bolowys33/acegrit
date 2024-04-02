5a4c8828a8ef21de88cabd16602c823a22a9df50
Olubodun <bolowys33@gmail.com>
2024-04-02 20:55:36 +0100

    Add contact div to contact page

--- app/contact-us/page.tsx
+++ app/contact-us/page.tsx
@@ -1,17 +1,37 @@
 import PageBanner from "@/components/PageBanner";
+import { AccessTimeOutlined, LocationOnOutlined, PermPhoneMsgOutlined } from "@mui/icons-material";
 import { Container } from "@mui/material";
 
 const Contact = () => {
     return (
         <div>
             <PageBanner title="Contact Us" classes="contact-image" />
-            <Container maxWidth="xl">
-                <div>
-                    <h3 className="text-center">Get in touch with us</h3>
-                    <div>
-                        <div></div>
-                        <div></div>
-                        <div></div>
+            <Container maxWidth="lg">
+                <div className="md:px-6">
+                <h3 className="text-[29px] text-[#222261] text-center font-extrabold mb-3">Get in touch with us</h3>
+                    <div className="flex flex-col gap-10 md:gap-0 md:flex-row justify-between border-y border-gold text-center py-10">
+                        <div>
+                            <PermPhoneMsgOutlined className="text-[#222261] text-[45px] mb-4" />
+                            <span>
+                                <p>Tel: +234111111111</p>
+                                <p>Email: email@email.com</p>
+                            </span>
+                        </div>
+                        <div>
+                            <LocationOnOutlined className="text-[#222261] text-[45px] mb-4"/>
+                            <span >
+                                <p>Tel: +234111111111</p>
+                                <p>Email: email@email.com</p>
+                            </span>
+                        </div>
+                        <div>
+                            <AccessTimeOutlined className="text-[#222261] text-[45px] mb-4"/>
+                            <span>
+                                <p>Tel: +234111111111</p>
+                                <p>Email: email@email.com</p>
+                            </span>
+                        </div>
+                        
                     </div>
                     <div></div>
                 </div>
