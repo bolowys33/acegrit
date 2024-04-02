ba1af293cb30f6e0296821829c7272b9d55b3ff2
Olubodun <bolowys33@gmail.com>
2024-04-02 19:58:17 +0100

    Aetting up contact page

--- app/contact-us/page.tsx
+++ app/contact-us/page.tsx
@@ -1,11 +1,24 @@
 import PageBanner from "@/components/PageBanner";
+import { Container } from "@mui/material";
 
 const Contact = () => {
-    return ( 
+    return (
         <div>
-            <PageBanner title="Contact Us" classes="contact-image"/>
+            <PageBanner title="Contact Us" classes="contact-image" />
+            <Container maxWidth="xl">
+                <div>
+                    <h3 className="text-center">Get in touch with us</h3>
+                    <div>
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                    </div>
+                    <div></div>
+                </div>
+            </Container>
+            <form action=""></form>
         </div>
-     );
-}
- 
+    );
+};
+
 export default Contact;
