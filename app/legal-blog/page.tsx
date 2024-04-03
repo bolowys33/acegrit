5abdcb5b14570e738ae8ca4fe0753ff123c14ada
Olubodun <bolowys33@gmail.com>
2024-04-03 11:20:29 +0100

    Update

--- app/legal-blog/page.tsx
+++ app/legal-blog/page.tsx
@@ -12,7 +12,7 @@ const Blog = () => {
                     <h3 className="text-[29px] md:text-[34px] text-start font-extrabold tracking-[1.5px]">
                         From the Desk
                     </h3>
-                    <p className="tracking-[.5px] text-justify font-[500] text-zinc-400">Our update pages attempts to keep you abreast with current legislations, policies, pronouncements and events within the legal space in Africa.</p>
+                    <p className="tracking-[.5px] font-[500] text-zinc-400">Our update pages attempts to keep you abreast with current legislations, policies, pronouncements and events within the legal space in Africa.</p>
                 </div>
             </Container>
         </div>
