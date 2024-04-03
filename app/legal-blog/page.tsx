8cdef71add6c25cd3df803f31eec4a6db1fa6b5a
Olubodun <bolowys33@gmail.com>
2024-04-03 11:01:59 +0100

    Set uo legal blog page

---
+++ app/legal-blog/page.tsx
@@ -0,0 +1,11 @@
+import PageBanner from "@/components/PageBanner";
+
+const Blog = () => {
+    return (
+        <div>
+            <PageBanner title="Legal Blog" classes="blog-image" />
+        </div>
+    );
+};
+
+export default Blog;
