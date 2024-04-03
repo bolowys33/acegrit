60808f16c6adbe9f32f5cb83c7797f2569c8f746
Olubodun <bolowys33@gmail.com>
2024-04-03 11:18:45 +0100

    Update

--- app/legal-blog/page.tsx
+++ app/legal-blog/page.tsx
@@ -1,4 +1,5 @@
 import PageBanner from "@/components/PageBanner";
+import SectionTitle from "@/components/SectionTitle";
 import { Container } from "@mui/material";
 
 const Blog = () => {
@@ -6,7 +7,13 @@ const Blog = () => {
         <div>
             <PageBanner title="Legal Blog" classes="blog-image" />
             <Container maxWidth="xl">
-                
+                <div className="md:px-5">
+                    <SectionTitle section="legal blog" classes="" />
+                    <h3 className="text-[29px] md:text-[34px] text-start font-extrabold tracking-[1.5px]">
+                        From the Desk
+                    </h3>
+                    <p className="tracking-[.5px] text-justify font-[500] text-zinc-400">Our update pages attempts to keep you abreast with current legislations, policies, pronouncements and events within the legal space in Africa.</p>
+                </div>
             </Container>
         </div>
     );
