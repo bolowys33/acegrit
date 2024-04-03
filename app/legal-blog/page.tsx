b261281524703af6ab1c9bbb82e1d8fbaa9e37f4
Olubodun <bolowys33@gmail.com>
2024-04-03 11:04:37 +0100

    Add container

--- app/legal-blog/page.tsx
+++ app/legal-blog/page.tsx
@@ -1,9 +1,13 @@
 import PageBanner from "@/components/PageBanner";
+import { Container } from "@mui/material";
 
 const Blog = () => {
     return (
         <div>
             <PageBanner title="Legal Blog" classes="blog-image" />
+            <Container maxWidth="xl">
+                
+            </Container>
         </div>
     );
 };
