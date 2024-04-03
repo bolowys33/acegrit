fc05f238e3e506b7ba0c2371ea38930d40dcb568
Olubodun <bolowys33@gmail.com>
2024-04-03 19:56:44 +0100

    Update

--- app/practice-areas/page.tsx
+++ app/practice-areas/page.tsx
@@ -1,9 +1,13 @@
 import PageBanner from "@/components/PageBanner";
 import SectionTitle from "@/components/SectionTitle";
+import { Container } from "@mui/material";
 
 const PracticeAreas = () => {
     return ( <div>
             <PageBanner title="Practice Areas" classes="area-image" />
+            <Container maxWidth="xl">
+                <SectionTitle section="practice areas" classes="" />
+            </Container>
         </div> );
 }
  
