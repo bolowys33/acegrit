8ddddfdd1f2e0e83110b074bb82c4b6c8352ef00
Olubodun <bolowys33@gmail.com>
2024-04-04 19:42:25 +0100

    Update

--- app/practice-areas/[area]/page.tsx
+++ app/practice-areas/[area]/page.tsx
@@ -17,16 +17,51 @@ const Area = ({ params }: { params: Areas }) => {
             <PageBanner title={link?.name} classes={`${area}-image`} />
             <Container maxWidth="xl">
                 <div className="md:px-5">
-                    <SectionTitle section="practice area" classes="text-center" />
+                    <SectionTitle
+                        section="practice area"
+                        classes="text-center"
+                    />
                     <h3 className="text-[29px] md:text-[34px] font-extrabold text-center tracking-[1.5px] mb-8">
                         {link?.name}
                     </h3>
-                    <Container maxWidth="md" className="font-poppins text-justify font-medium text-zinc-700">
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur quibusdam delectus, facere quod tempora inventore dolores culpa saepe, non reiciendis doloremque dolore consequuntur nobis, blanditiis nulla. Quam sint ullam voluptatibus?</p>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur quibusdam delectus, facere quod tempora inventore dolores culpa saepe, non reiciendis doloremque dolore consequuntur nobis, blanditiis nulla. Quam sint ullam voluptatibus?</p>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur quibusdam delectus, facere quod tempora inventore dolores culpa saepe, non reiciendis doloremque dolore consequuntur nobis, blanditiis nulla. Quam sint ullam voluptatibus?</p>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur quibusdam delectus, facere quod tempora inventore dolores culpa saepe, non reiciendis doloremque dolore consequuntur nobis, blanditiis nulla. Quam sint ullam voluptatibus?</p>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur quibusdam delectus, facere quod tempora inventore dolores culpa saepe, non reiciendis doloremque dolore consequuntur nobis, blanditiis nulla. Quam sint ullam voluptatibus?</p>
+                    <Container
+                        maxWidth="md"
+                        className="font-poppins text-justify font-medium text-zinc-700">
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing
+                            elit. Aspernatur quibusdam delectus, facere quod
+                            tempora inventore dolores culpa saepe, non
+                            reiciendis doloremque dolore consequuntur nobis,
+                            blanditiis nulla. Quam sint ullam voluptatibus?
+                        </p>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing
+                            elit. Aspernatur quibusdam delectus, facere quod
+                            tempora inventore dolores culpa saepe, non
+                            reiciendis doloremque dolore consequuntur nobis,
+                            blanditiis nulla. Quam sint ullam voluptatibus?
+                        </p>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing
+                            elit. Aspernatur quibusdam delectus, facere quod
+                            tempora inventore dolores culpa saepe, non
+                            reiciendis doloremque dolore consequuntur nobis,
+                            blanditiis nulla. Quam sint ullam voluptatibus?
+                        </p>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing
+                            elit. Aspernatur quibusdam delectus, facere quod
+                            tempora inventore dolores culpa saepe, non
+                            reiciendis doloremque dolore consequuntur nobis,
+                            blanditiis nulla. Quam sint ullam voluptatibus?
+                        </p>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing
+                            elit. Aspernatur quibusdam delectus, facere quod
+                            tempora inventore dolores culpa saepe, non
+                            reiciendis doloremque dolore consequuntur nobis,
+                            blanditiis nulla. Quam sint ullam voluptatibus?
+                        </p>
                     </Container>
                 </div>
             </Container>
