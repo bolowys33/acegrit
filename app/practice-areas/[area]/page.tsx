2181feaa44e5bdf172a0d7f94d8ad2707153cdc1
Olubodun <bolowys33@gmail.com>
2024-04-04 13:12:57 +0100

    Add session title

--- app/practice-areas/[area]/page.tsx
+++ app/practice-areas/[area]/page.tsx
@@ -1,5 +1,7 @@
 import PageBanner from "@/components/PageBanner";
+import SectionTitle from "@/components/SectionTitle";
 import { practiceAreas } from "@/constants/links";
+import { Container } from "@mui/material";
 
 interface Areas {
     area: string;
@@ -7,12 +9,20 @@ interface Areas {
 
 const Area = ({ params }: { params: Areas }) => {
     const { area } = params;
- 
-    const link = practiceAreas.find(item => item.path === `/${area}`)
+
+    const link = practiceAreas.find((item) => item.path === `/${area}`);
 
     return (
         <div>
             <PageBanner title={link?.name} classes={`${area}-image`} />
+            <Container maxWidth="xl">
+                <div className="md:px-5">
+                    <SectionTitle section="practice area" />
+                    <h3 className="text-[29px] md:text-[34px] font-extrabold tracking-[1.5px] mb-8">
+                        {link?.name}
+                    </h3>
+                </div>
+            </Container>
         </div>
     );
 };
