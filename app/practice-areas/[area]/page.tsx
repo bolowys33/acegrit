dc1eeaee49d33ab57436556877c2650e1e23facd
Olubodun <bolowys33@gmail.com>
2024-04-04 20:19:32 +0100

    Add other areas to practice areas component

--- app/practice-areas/[area]/page.tsx
+++ app/practice-areas/[area]/page.tsx
@@ -4,11 +4,8 @@ import { practiceAreas } from "@/constants/links";
 import { Container } from "@mui/material";
 import Image from "next/image";
 import Link from "next/link";
-import commercial from "@/public/assets/commercial.jpg";
-import litigation from "@/public/assets/litigation.jpg";
-import conflict from "@/public/assets/conflict.jpg";
-import probate from "@/public/assets/probate.jpg";
-import PracticeAreas from "../page";
+import Social from "@/components/Social";
+import OtherAreas from "@/components/OtherAreas";
 
 interface Areas {
     area: string;
@@ -17,16 +14,7 @@ interface Areas {
 const Area = ({ params }: { params: Areas }) => {
     const { area } = params;
     const link = practiceAreas.find((item) => item.path === `/${area}`);
-    const images = [litigation, probate, commercial, conflict];
-    const linkAreas = practiceAreas.filter((item) => item.path !== `/${area}`);
-    const filteredIndex = practiceAreas.findIndex(
-        (item) => item.path === `/${area}`
-    );
-
-    if (filteredIndex !== -1) {
-        images.splice(filteredIndex, 1);
-    }
-
+    
     return (
         <div>
             <PageBanner title={link?.name} classes={`${area}-image`} />
@@ -78,37 +66,10 @@ const Area = ({ params }: { params: Areas }) => {
                             blanditiis nulla. Quam sint ullam voluptatibus?
                         </p>
                     </Container>
-                    <div className="">
-                        <div className="text-center mb-6">
-                            <SectionTitle section="practice areas" />
-                            <h3 className="text-[29px] md:text-[34px] font-extrabold mb-3">
-                                Other Practice Areas
-                            </h3>
-                            <span className="text-2xl text-[#222261]">|</span>
-                        </div>
-                        <div className="flex flex-col md:flex-row  gap-4 p-10 bg-zinc-200">
-                            {linkAreas.map((area, index) => (
-                                <div
-                                    key={area.path}
-                                    className="bg-white shadow-lg rounded-md p-3 col-span-full md:col-span-6 lg:col-span-3">
-                                    <Link href={area.path} className="relative">
-                                        <Image
-                                            src={images[index]}
-                                            alt="area.name"
-                                            height={700}
-                                        />
-                                        <div className="bg-tansparent w-full h-full absolute top-0 hover:bg-black hover:bg-opacity-40 transition duration-700 ease-in-out"></div>
-                                    </Link>
-                                    <SectionTitle
-                                        section={area.name}
-                                        classes="text-center"
-                                    />
-                                </div>
-                            ))}
-                        </div>
-                    </div>
                 </div>
             </Container>
+            <OtherAreas area={area} />
+            <Social />
         </div>
     );
 };
---
+++ components/OtherAreas.tsx
@@ -0,0 +1,59 @@
+import Image from "next/image";
+import Link from "next/link";
+import SectionTitle from "./SectionTitle";
+import { practiceAreas } from "@/constants/links";
+import { FC } from "react";
+import commercial from "@/public/assets/commercial.jpg";
+import litigation from "@/public/assets/litigation.jpg";
+import conflict from "@/public/assets/conflict.jpg";
+import probate from "@/public/assets/probate.jpg";
+
+interface OtherAreasProps {
+    area?: string;
+}
+
+const OtherAreas: FC<OtherAreasProps> = ({ area }) => {
+    const images = [litigation, probate, commercial, conflict];
+    const linkAreas = practiceAreas.filter((item) => item.path !== `/${area}`);
+    const filteredIndex = practiceAreas.findIndex(
+        (item) => item.path === `/${area}`
+    );
+
+    if (filteredIndex !== -1) {
+        images.splice(filteredIndex, 1);
+    }
+
+    return (
+        <div className="">
+            <div className="text-center mb-6">
+                <SectionTitle section="practice areas" />
+                <h3 className="text-[29px] md:text-[34px] font-extrabold mb-3">
+                    Other Practice Areas
+                </h3>
+                <span className="text-2xl text-[#222261]">|</span>
+            </div>
+            <div className="flex flex-col md:flex-row  gap-4 p-10 bg-zinc-200">
+                {linkAreas.map((area, index) => (
+                    <div
+                        key={area.path}
+                        className="bg-white shadow-lg rounded-md p-3 col-span-full md:col-span-6 lg:col-span-3">
+                        <Link href={area.path} className="relative">
+                            <Image
+                                src={images[index]}
+                                alt="area.name"
+                                height={700}
+                            />
+                            <div className="bg-tansparent w-full h-full absolute top-0 hover:bg-black hover:bg-opacity-40 transition duration-700 ease-in-out"></div>
+                        </Link>
+                        <SectionTitle
+                            section={area.name}
+                            classes="text-center"
+                        />
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+};
+
+export default OtherAreas;
