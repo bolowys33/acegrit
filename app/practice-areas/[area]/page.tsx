374788ade73337a0101db660ed38751fe070d0b7
Olubodun <bolowys33@gmail.com>
2024-04-04 12:40:29 +0100

    Add Page banner

--- app/practice-areas/[area]/page.tsx
+++ app/practice-areas/[area]/page.tsx
@@ -1,12 +1,21 @@
+import PageBanner from "@/components/PageBanner";
+import { practiceAreas } from "@/constants/links";
+
 interface Areas {
     area: string;
 }
 
 const Area = ({ params }: { params: Areas }) => {
     const { area } = params;
+ 
+    const link = practiceAreas.find(item => item.path === `/${area}`)
+    console.log(area);
+    
+    
+
     return (
         <div>
-            <h1>area: {area}</h1>
+            <PageBanner title={link?.name} classes="" />
         </div>
     );
 };
