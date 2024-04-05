fac4a641995c679293a2f018b43df076b0ebfbc3
Olubodun <bolowys33@gmail.com>
2024-04-05 21:07:32 +0100

    Add image to carousel

--- components/Slider.tsx
+++ components/Slider.tsx
@@ -1,12 +1,35 @@
 import { CCarousel, CCarouselCaption, CCarouselItem, CImage } from "@coreui/react";
+import litigation from "@/public/assets/litigation.jpg"
 
 const Slider = () => {
     return ( <div>
             <CCarousel>
                 <CCarouselItem>
-                    <CImage />
+                    <CImage src={litigation.src} />
                     <CCarouselCaption>
-
+                        <h3>Welcome</h3>
+                        <p>Ace & Grit LP</p>
+                    </CCarouselCaption>
+                </CCarouselItem>
+                <CCarouselItem>
+                    <CImage src={litigation.src} />
+                    <CCarouselCaption>
+                        <h3>Welcome</h3>
+                        <p>Ace & Grit LP</p>
+                    </CCarouselCaption>
+                </CCarouselItem>
+                <CCarouselItem>
+                    <CImage src={litigation.src} />
+                    <CCarouselCaption>
+                        <h3>Welcome</h3>
+                        <p>Ace & Grit LP</p>
+                    </CCarouselCaption>
+                </CCarouselItem>
+                <CCarouselItem>
+                    <CImage src={litigation.src} />
+                    <CCarouselCaption>
+                        <h3>Welcome</h3>
+                        <p>Ace & Grit LP</p>
                     </CCarouselCaption>
                 </CCarouselItem>
             </CCarousel>
