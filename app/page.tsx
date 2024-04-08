603e0ff5603a267ca63ae1764072bc5f2020ed67
Olubodun <bolowys33@gmail.com>
2024-04-08 12:00:23 +0100

    Add expertise to home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
+import Expertise from "@/components/Expertise";
 import Slider from "@/components/Slider";
 
 const Home = () => {
     return <div>
         <Slider />
+        <Expertise />
     </div>;
 };
 
