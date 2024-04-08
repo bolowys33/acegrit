cedd03772e71c0ed5521acfd6c5dfc8522efe7c2
Olubodun <bolowys33@gmail.com>
2024-04-08 22:23:42 +0100

    Update

--- app/attorneys/page.tsx
+++ app/attorneys/page.tsx
@@ -1,6 +1,8 @@
 import PageBanner from "@/components/PageBanner";
 import SectionTitle from "@/components/SectionTitle";
 import { Container } from "@mui/material";
+import Image from "next/image";
+import litigation from "@/public/assets/litigation.jpg"
 
 const Attorneys = () => {
     return (
@@ -8,10 +10,15 @@ const Attorneys = () => {
             <PageBanner title="Attorneys" classes="people-image" />
             <Container>
                 <div className="mx-auto text-center">
-                <SectionTitle section="Our team" />
-                <h3 className="text-[29px] md:text-[34px] font-extrabold mb-3">
-                    Meet our team
-                </h3>
+                    <SectionTitle section="Our team" />
+                    <h3 className="text-[29px] md:text-[34px] font-extrabold mb-3">
+                        Meet our team
+                    </h3>
+                </div>
+                <div>
+                    <Image src={litigation} alt="" className="w-[700px]" />
+                    <SectionTitle section="Bolodeoku babajide" />
+                    <SectionTitle section="managing partner" />
                 </div>
             </Container>
         </div>
