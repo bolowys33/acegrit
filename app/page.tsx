d8b6208c69a58d500d8bd7607ff1b2d6cd7c45d7
Olubodun <bolowys33@gmail.com>
2024-04-08 12:01:28 +0100

    Add social links to home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Expertise from "@/components/Expertise";
 import Slider from "@/components/Slider";
+import Social from "@/components/Social";
 
 const Home = () => {
     return <div>
         <Slider />
         <Expertise />
+        <Social classes="bg-zinc-200" />
     </div>;
 };
 
