34dba391db5b2d9897d3b2be5c0b0e6da8ec19e8
Olubodun <bolowys33@gmail.com>
2024-04-08 20:57:14 +0100

    Add abpot component to home page

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,12 @@ import Blog from "@/components/Blog";
 import Expertise from "@/components/Expertise";
 import Slider from "@/components/Slider";
 import Social from "@/components/Social";
+import About from "./about/page";
 
 const Home = () => {
     return <div>
         <Slider />
+        <About />
         <Blog />
         <Expertise />
         <Social />
