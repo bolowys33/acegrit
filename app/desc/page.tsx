a76c478594c518e02019f6dd5f05e6b49ab8278d
Olubodun <bolowys33@gmail.com>
2024-04-10 20:12:10 +0100

    Add page banner to blog content page

--- app/desc/page.tsx
+++ app/desc/page.tsx
@@ -1,5 +1,9 @@
+import PageBanner from "@/components/PageBanner";
+
 const BlogContent = () => {
-    return (  );
+    return ( <div>
+            <PageBanner title="Immovable Assets as Securities; The New Era" />
+        </div> );
 }
  
 export default BlogContent;
