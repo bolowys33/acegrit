ec289e61d84270020ec2fba4e6543270e4d6529d
Olubodun <bolowys33@gmail.com>
2024-04-10 20:19:01 +0100

    Update

--- app/desc/page.tsx
+++ app/desc/page.tsx
@@ -1,9 +1,17 @@
 import PageBanner from "@/components/PageBanner";
 
 const BlogContent = () => {
-    return ( <div>
-            <PageBanner title="Immovable Assets as Securities; The New Era" classes="" />
-        </div> );
-}
- 
+    return (
+        <div>
+            <div className={`bg-cover bg-center text-center mb-10`}>
+                <div className="bg-navy bg-opacity-40 py-[100px] md:py-[180px]">
+                    <h1 className="text-white font-main font-bold text-2xl md:text-[50px] mx-6">
+                        Immovable Assets as Securities; The New Era
+                    </h1>
+                </div>
+            </div>
+        </div>
+    );
+};
+
 export default BlogContent;
