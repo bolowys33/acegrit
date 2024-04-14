fc908d4aeaca43d768fe07669d57885457993909
Olubodun <bolowys33@gmail.com>
2024-04-14 18:52:44 +0100

    Update

--- lib/models/post.model.ts
+++ lib/models/post.model.ts
@@ -2,5 +2,13 @@ import { Schema } from "mongoose";
 
 
 const postSchema = new Schema({
+    title: {
+        type: String,
+        required: [true, "Post title is required"],
+    },
+    content: {
+        type: String,
+        required: [true, "Post content is required"]
+    },
     
 })
