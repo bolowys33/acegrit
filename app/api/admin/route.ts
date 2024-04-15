a4660d8a0b64abbfc0909d30f71944a2145b81d7
Olubodun <bolowys33@gmail.com>
2024-04-15 20:27:53 +0100

    Update

--- app/api/admin/route.ts
+++ app/api/admin/route.ts
@@ -1,8 +1,18 @@
 import connectDB from "@/lib/db";
-import { NextRequest, NextResponse } from "next/server";
+import Admin from "@/lib/models/admin.model";
+import { NextApiRequest, NextApiResponse } from "next";
 
-const routeHandler = async (req: NextRequest, res: NextResponse) => {
+const routeHandler = async (req: NextApiRequest, res: NextApiResponse) => {
     await connectDB();
 };
 
-
+const adminRegister = async (req: NextApiRequest, res: NextApiResponse) => {
+    try {
+        const { username, password, email } = req.body;
+        const admin = new Admin({ username, password, email });
+        await admin.save();
+        res.status(201).json({ message: "Admin registered successfully" });
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
+};
