8f3ffdb8e9db6f4678be758e01916bddf9a43c5f
Olubodun <bolowys33@gmail.com>
2024-04-16 20:16:40 +0100

    Update

--- app/api/attorney/route.ts
+++ app/api/attorney/route.ts
@@ -1,11 +1,28 @@
 import { Response } from "@/constants/response";
 import connectDB from "@/lib/db";
+import Attorney from "@/lib/models/attorney.model";
 import { NextApiRequest, NextApiResponse } from "next";
 
 
 const routeHandler = async (req: NextApiRequest, res: NextApiResponse) => {
     await connectDB()
 }
+
+async function getAttorneys(req: NextApiRequest, res: NextApiResponse<Response>) {
+    try {
+        const attorneys = await Attorney.find({}).select('-__v')
+
+        if (attorneys.length === 0) {
+            return res.status(404).json({success: false, message: "No attorney found"})
+        }
+
+        re
+
+    } catch (error) {
+        
+    }
+}
+
 
 async function addAttorney(req: NextApiRequest, res: NextApiResponse<Response>) {
     
