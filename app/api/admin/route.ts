2237873e3099c2aa95e36dcc1175eef992e37146
Olubodun <bolowys33@gmail.com>
2024-04-16 11:53:15 +0100

    Update

--- app/api/admin/route.ts
+++ app/api/admin/route.ts
@@ -1,27 +1,26 @@
+import { Response } from "@/constants/response";
 import connectDB from "@/lib/db";
 import Admin from "@/lib/models/admin.model";
 import { NextApiRequest, NextApiResponse } from "next";
 
-interface Response {
-    success: boolean;
-    message: string
-}
 
 const routeHandler = async (req: NextApiRequest, res: NextApiResponse) => {
     await connectDB();
 };
 
-const adminRegister = async (req: NextApiRequest, res: NextApiResponse) => {
+const adminRegister = async (req: NextApiRequest, res: NextApiResponse<Response>) => {
     try {
         const { username, password, email } = req.body;
+
         if (!username || !password || !email) {
-            res.status(400).json()
+            res.status(400).json({success: false, message: "Please provide all required fields"})
         }
 
         const admin = new Admin({ username, password, email });
         await admin.save();
-        res.status(201).json({ message: "Admin registered successfully" });
+        
+        res.status(201).json({success: true, message: "Admin registered successfully" });
     } catch (error) {
-        res.status(500).json({ message: (error as Error).message });
+        res.status(500).json({success: false, message: (error as Error).message });
     }
 };
