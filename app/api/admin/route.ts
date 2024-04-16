8ef78dfbe3824bdb1b87f15f6fbdbada56c42091
Olubodun <bolowys33@gmail.com>
2024-04-16 11:45:43 +0100

    Add interface

--- app/api/admin/route.ts
+++ app/api/admin/route.ts
@@ -2,6 +2,11 @@ import connectDB from "@/lib/db";
 import Admin from "@/lib/models/admin.model";
 import { NextApiRequest, NextApiResponse } from "next";
 
+interface Response {
+    success: boolean;
+    message: string
+}
+
 const routeHandler = async (req: NextApiRequest, res: NextApiResponse) => {
     await connectDB();
 };
@@ -9,10 +14,14 @@ const routeHandler = async (req: NextApiRequest, res: NextApiResponse) => {
 const adminRegister = async (req: NextApiRequest, res: NextApiResponse) => {
     try {
         const { username, password, email } = req.body;
+        if (!username || !password || !email) {
+            res.status(400).json()
+        }
+
         const admin = new Admin({ username, password, email });
         await admin.save();
         res.status(201).json({ message: "Admin registered successfully" });
     } catch (error) {
-        res.status(400).json({ message: (error as Error).message });
+        res.status(500).json({ message: (error as Error).message });
     }
 };
