1f4d4814fbc70ba47c480521d39651dc54ac07fa
Olubodun <bolowys33@gmail.com>
2024-04-18 20:42:22 +0100

    Update

--- app/api/login/route.ts
+++ app/api/login/route.ts
@@ -23,10 +23,14 @@ async function adminLogin(
     }
 
     const payload = {id: admin._id, username: admin.username, email: admin.email}
-    const token = jwt.sign(payload, secret, { expiresIn: '2h' })
+    const token = jwt.sign(payload, secret as string, { expiresIn: '2h' })
 
    } catch (error) {
-    
+        if (error instanceof Error) {
+            return res.status(400).json({ success: false, message: error.message });
+        } else {
+            return res.status(500).json({ success: false, message: "An unknown error occurred" });
+        }
    }
 
 }
