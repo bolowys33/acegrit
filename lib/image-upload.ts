60881a0b1f30e357d44c4b05af3982a8f257be4a
Olubodun <bolowys33@gmail.com>
2024-04-18 23:43:18 +0100

    Update

--- lib/image-upload.ts
+++ lib/image-upload.ts
@@ -1,8 +1,8 @@
-import multer from "multer"
-import cloudinary from "cloudinary"
+import multer from "multer";
+import { v2 as cloudinary } from "cloudinary";
 
-cloudinary.v2.config({
+cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
     api_key: process.env.CLOUDINARY_API_KEY,
     api_secret: process.env.CLOUDINARY_API_SECRET,
-  });
+});
