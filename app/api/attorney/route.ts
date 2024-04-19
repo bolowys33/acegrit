dc6b032e62a7b26a9a3a4e1c9f3ddbd2a0e6afe5
Olubodun <bolowys33@gmail.com>
2024-04-19 22:12:54 +0100

    Update

--- app/api/attorney/route.ts
+++ app/api/attorney/route.ts
@@ -151,7 +151,11 @@ async function removeAttorney(req: NextApiRequest, res: NextApiResponse<Response
 
     return res.status(200).json({ success: true, message: "Attorney updated successfully"})
    } catch (error) {
-    
+    if (error instanceof Error) {
+        return res.status(400).json({ success: false, message: error.message });
+      } else {
+        return res.status(500).json({ success: false, message: "An unknown error occurred" });
+      }
    }
 
 }
