92111619dc903eb5e355ad0f48694f518b8dc00e
Olubodun <bolowys33@gmail.com>
2024-04-20 00:00:09 +0100

    Convert login to using app router

--- app/api/admin/route.ts
+++ app/api/admin/route.ts
@@ -2,54 +2,68 @@ import { NextResponse } from "next/server";
 import connectDB from "@/lib/db";
 import Admin from "@/lib/models/admin.model";
 
-
 export async function POST(request: Request): Promise<Response> {
-  await connectDB();
+    await connectDB();
 
-  try {
-    const { username, password, email } = await request.json();
+    try {
+        const { username, password, email } = await request.json();
 
-    if (!username || !password || !email) {
-      return NextResponse.json(
-        { success: false, message: "Please provide all required fields" },
-        { status: 400 }
-      );
-    }
+        if (!username || !password || !email) {
+            return NextResponse.json(
+                {
+                    success: false,
+                    message: "Please provide all required fields",
+                },
+                { status: 400 }
+            );
+        }
 
-    const existingAdmin = await Admin.findOne({ $or: [{ email }, { username }] });
+        const existingAdmin = await Admin.findOne({
+            $or: [{ email }, { username }],
+        });
 
-    if (existingAdmin) {
-      if (existingAdmin.email === email) {
+        if (existingAdmin) {
+            if (existingAdmin.email === email) {
+                return NextResponse.json(
+                    {
+                        success: false,
+                        message: "Admin with email address already exists",
+                    },
+                    { status: 400 }
+                );
+            } else {
+                return NextResponse.json(
+                    {
+                        success: false,
+                        message: "Admin with username already exists",
+                    },
+                    { status: 400 }
+                );
+            }
+        }
+
+        const admin = new Admin({ username, password, email });
+        await admin.save();
+
         return NextResponse.json(
-          { success: false, message: "Admin with email address already exists" },
-          { status: 400 }
+            {
+                success: true,
+                message: "Admin registered successfully",
+                data: admin,
+            },
+            { status: 201 }
         );
-      } else {
-        return NextResponse.json(
-          { success: false, message: "Admin with username already exists" },
-          { status: 400 }
-        );
-      }
+    } catch (error) {
+        if (error instanceof Error) {
+            return NextResponse.json(
+                { success: false, message: error.message },
+                { status: 400 }
+            );
+        } else {
+            return NextResponse.json(
+                { success: false, message: "An unknown error occurred" },
+                { status: 500 }
+            );
+        }
     }
-
-    const admin = new Admin({ username, password, email });
-    await admin.save();
-
-    return NextResponse.json(
-      { success: true, message: "Admin registered successfully", data: admin },
-      { status: 201 }
-    );
-  } catch (error) {
-    if (error instanceof Error) {
-      return NextResponse.json(
-        { success: false, message: error.message },
-        { status: 400 }
-      );
-    } else {
-      return NextResponse.json(
-        { success: false, message: "An unknown error occurred" },
-        { status: 500 }
-      );
-    }
-  }
 }
