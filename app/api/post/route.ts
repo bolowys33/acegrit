cbe5a3c65c54f3878d64ff78b6cd269089d42f1d
Olubodun <bolowys33@gmail.com>
2024-04-22 15:56:43 +0100

    Update

--- app/api/post/route.ts
+++ app/api/post/route.ts
@@ -32,3 +32,21 @@ export async function GET(): Promise<Response> {
         }
     }
 }
+
+export async function POST(): Promise<Response> {
+    try {
+        await connectDB()
+    } catch (error) {
+        if (error instanceof Error) {
+            return NextResponse.json(
+                { success: false, message: error.message },
+                { status: 400 }
+            );
+        } else {
+            return NextResponse.json(
+                { success: false, message: "An unknown error occurred" },
+                { status: 500 }
+            );
+        }
+    }
+}
