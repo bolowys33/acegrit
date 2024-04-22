47f869814a8b4fbcfffe82ec6a7f0fb7084f0ec6
Olubodun <bolowys33@gmail.com>
2024-04-22 15:48:23 +0100

    Update

--- app/api/post/route.ts
+++ app/api/post/route.ts
@@ -1,12 +1,17 @@
 import connectDB from "@/lib/db";
-
+import Post from "@/lib/models/post.model";
+import { NextResponse } from "next/server";
 
 export async function GET(): Promise<Response> {
     try {
-        await connectDB()
+        await connectDB();
 
-        
-    } catch (error) {
-        
-    }
+        const post = await Post.find({});
+        if (post.length === 0) {
+            return NextResponse.json(
+                { success: false, message: "No post found" },
+                { status: 404 }
+            );
+        }
+    } catch (error) {}
 }
