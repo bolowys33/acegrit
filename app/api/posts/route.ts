a605dacc24c8a45e4d91902b7e7da2c66ed3ed35
Olubodun <bolowys33@gmail.com>
2024-04-23 13:47:40 +0100

    Update

--- app/api/posts/route.ts
+++ app/api/posts/route.ts
@@ -6,7 +6,9 @@ export async function GET(): Promise<Response> {
     try {
         await connectDB();
 
-        const posts = await Post.find({}).select("-__v");
+        const posts = await Post.find({})
+            .select("-__v")
+            .populate({ path: "author", select: "firstname lastname" });
         if (posts.length === 0) {
             return NextResponse.json(
                 { success: false, message: "No post found" },
@@ -69,7 +71,12 @@ export async function POST(req: Request): Promise<Response> {
             .replace(/[^a-z0-9]+/g, "-")
             .replace(/^-+|-+$/g, "");
 
-        const post = new Post({ title, content, post_url: postUrl, author: adminId });
+        const post = new Post({
+            title,
+            content,
+            post_url: postUrl,
+            author: adminId,
+        });
         await post.save();
 
         return NextResponse.json(
