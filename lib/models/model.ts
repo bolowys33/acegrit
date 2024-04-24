cf5b8714bb9bbd22d792b331f6a5f577fcee3d4c
Olubodun <bolowys33@gmail.com>
2024-04-24 10:25:44 +0100

    Define models with exported schemas

--- lib/models/model.ts
+++ lib/models/model.ts
@@ -1,7 +1,10 @@
-import { model } from "mongoose";
-import { models } from "mongoose";
+import { model, models } from "mongoose";
+import { adminSchema } from "./admin.schema";
+import { attorneySchema } from "./attorney.schema";
+import { postSchema } from "./post.schema";
 
-
-const Admin = models.Admin || model("Admin", adminSchema)
+const Admin = models.Admin || model("Admin", adminSchema);
 const Attorney = models.Attorney || model("Attorney", attorneySchema);
 const Post = models.Post || model("Post", postSchema);
+
+export { Admin, Attorney, Post };
