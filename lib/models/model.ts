b3047e3563a7cbb1efbd6965d787dc1b3ac6ba15
Olubodun <bolowys33@gmail.com>
2024-04-24 13:00:24 +0100

    Update

--- lib/models/model.ts
+++ lib/models/model.ts
@@ -2,9 +2,11 @@ import { model, models } from "mongoose";
 import { adminSchema } from "./admin.schema";
 import { attorneySchema } from "./attorney.schema";
 import { postSchema } from "./post.schema";
+import { tokenSchema } from "./token.schema";
 
 const Admin = models.Admin || model("Admin", adminSchema);
 const Attorney = models.Attorney || model("Attorney", attorneySchema);
 const Post = models.Post || model("Post", postSchema);
+const Token = models.Token || model("Token", tokenSchema);
 
 export { Admin, Attorney, Post };
