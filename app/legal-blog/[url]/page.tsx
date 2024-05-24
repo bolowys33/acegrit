5e098dfe8f2675f5b7045252af37fdad44ce3fd3
Olubodun <bolowys33@gmail.com>
2024-05-24 16:32:37 +0100

    Update

--- app/legal-blog/[url]/page.tsx
+++ app/legal-blog/[url]/page.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import useSinglePost from "@/hooks/useSinglePost";
 import { Container } from "@mui/material";
@@ -10,20 +10,22 @@ const BlogContent = ({ params }: { params: { url: string } }) => {
     return (
         <div>
             <div className={`bg-cover bg-center text-center mb-10 blog-image`}>
-                <div className="bg-black bg-opacity-45 py-[100px] md:py-[180px]">
-                    <h1 className="text-white font-main font-bold text-2xl md:text-[50px] mx-6">
+                <div className="bg-black bg-opacity-60 py-[100px] md:py-[180px]">
+                    <h1 className="text-white font-main font-bold text-2xl md:text-[50px] mx-6 leading-10">
                         {post?.title}
                     </h1>
                 </div>
             </div>
             <Container
-                maxWidth="md"
-                className="space-y-4 font-poppins text-justify font-medium text-zinc-600">
+                maxWidth="lg"
+                className="w-full flex flex-col-reverse md:flex-row gap-5 lg:gap-10 font-poppins mb-6 text-justify font-medium text-zinc-600">
                 <div
+                    className="flex-[2] lg:flex-[3]"
                     dangerouslySetInnerHTML={{
                         __html: post?.content ?? "",
                     }}
                 />
+                <div className="flex-[1]">jhdsjkbdshjxokdp[kfjodjcjxbcjbuxc</div>
             </Container>
         </div>
     );
