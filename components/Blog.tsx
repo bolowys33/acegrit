d8e82774e0d9997759551dfd0968b0155beaf009
Olubodun <bolowys33@gmail.com>
2024-05-24 16:52:17 +0100

    Update

--- components/Blog.tsx
+++ components/Blog.tsx
@@ -3,6 +3,7 @@
 import usePosts from "@/hooks/usePosts";
 import { Container } from "@mui/material";
 import Link from "next/link";
+import BlogSection from "./BlogSection";
 
 const Blog = () => {
     const { posts, isFetching, error } = usePosts();
@@ -39,53 +40,7 @@ const Blog = () => {
                     current legislations, policies, pronouncements, and events
                     within the legal space in Africa.
                 </p>
-                <section className="space-y-16 mb-10">
-                    {posts.map((post) => {
-                        const [month, day, year] = formatDate(
-                            post.date_created
-                        );
-                        return (
-                            <article key={post._id}>
-                                <div className="flex">
-                                    <div className="py-0 pr-4 border-r-2 h-18 border-navy tracking-[2px] hidden md:block">
-                                        <div className="flex flex-col items-center">
-                                            <p className="font-semibold uppercase">
-                                                {month}
-                                            </p>
-                                            <p className="text-[27px] font-extrabold leading-5 my-1">
-                                                {day}
-                                            </p>
-                                            <p className="font-semibold">
-                                                {year}
-                                            </p>
-                                        </div>
-                                    </div>
-                                    <div className="md:pl-4">
-                                        <Link
-                                            href="#"
-                                            className="text-[24px] md:text-[28px] text-start font-extrabold hover:text-navy">
-                                            {post.title}
-                                        </Link>
-                                    </div>
-                                </div>
-                                <div
-                                    className="text-zinc-400 my-6"
-                                    dangerouslySetInnerHTML={{
-                                        __html: `${post.content
-                                            .split(" ")
-                                            .slice(0, 15)
-                                            .join(" ")}...`,
-                                    }}
-                                />
-                                <Link
-                                    href={`/legal-blog/${post.post_url}`}
-                                    className="transition duration-300 ease-in-out bg-navy hover:bg-transparent hover:text-navy hover:border-navy border text-white py-2 px-4 font-semibold uppercase">
-                                    read more
-                                </Link>
-                            </article>
-                        );
-                    })}
-                </section>
+                <BlogSection posts={posts} />
                 <Link
                     href="/legal-blog"
                     className="self-center text-[20px] transition duration-300 ease-in-out bg-navy hover:bg-transparent hover:text-navy hover:border-navy border text-white py-2 px-4 font-semibold uppercase">
