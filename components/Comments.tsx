dcbf3c8a23f1d8633fcd72276362f574905d3c03
Olubodun <bolowys33@gmail.com>
2024-05-26 18:28:24 +0100

    Update

--- components/Comments.tsx
+++ components/Comments.tsx
@@ -1,7 +1,7 @@
 const Comments = () => {
     return (
         <div className="border-t border-gray my-12 py-5">
-            <h3 className="text-navy text-2xl font-extrabold mb-7" >2 Comments</h3>
+            <h3 className="text-navy text-2xl font-extrabold mb-7">Comments</h3>
             <article className="flex gap-5 ml-5 py-6 border-b border-gray ">
                 <div className="grid place-items-center h-12 w-12 bg-slate-400 rounded-full text-xl">
                     J
