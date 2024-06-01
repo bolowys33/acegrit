228d08cbb4644ef8110438869cb144d4dbe4be77
Olubodun <bolowys33@gmail.com>
2024-06-01 11:03:30 +0100

    Update

--- app/legal-blog/[url]/page.tsx
+++ app/legal-blog/[url]/page.tsx
@@ -12,18 +12,29 @@ const BlogContent = ({ params }: { params: { url: string } }) => {
     return (
         <div>
             <div className={`bg-cover bg-center text-center mb-10 blog-image`}>
-                <div className="bg-black bg-opacity-60 py-[100px] md:py-[180px]">
-                    <h1 className="text-white font-main font-bold text-2xl md:text-[50px] mx-6 leading-none">
+                <div className="bg-black bg-opacity-60 py-[100px] md:py-[180px] space-y-8">
+                    <h1 className="text-white font-main font-bold text-2xl md:text-[50px] mx-6 leading-normal">
                         {post?.title}
                     </h1>
+                    <div className="space-x-6 text-white md:text-xl font-medium">
+                        <span>23, Apr 2024</span>
+                        <span>*</span>
+                        <span>
+                            {post && post.comments && post.comments.length === 0
+                                ? "No"
+                                : post?.comments.length}{" "}
+                            {post && post.comments && post.comments.length < 2
+                                ? "comment"
+                                : "comments"}
+                        </span>
+                    </div>
                 </div>
             </div>
             <Container
                 maxWidth="lg"
                 className="w-full flex flex-col-reverse md:flex-row gap-5 lg:gap-10 font-poppins mb-6 text-justify font-medium text-zinc-600">
-                <div>
+                <div className="flex-[3] lg:flex-[4]">
                     <div
-                        className="flex-[2] lg:flex-[3]"
                         dangerouslySetInnerHTML={{
                             __html: post?.content ?? "",
                         }}
@@ -34,7 +45,6 @@ const BlogContent = ({ params }: { params: { url: string } }) => {
                 <div className="flex-[1]">
                     <p className="text-navy font-bold text-lg">Author</p>
                     <span className="font-bold">
-                        Babajide Bolodeoku
                         {post?.author.firstname} {post?.author.lastname}
                     </span>
                 </div>
