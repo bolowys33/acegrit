6b98d569d57a4e8b7f12f4aa245b228c5fe0132c
Olubodun <bolowys33@gmail.com>
2024-06-06 19:06:49 +0100

    Implment pagination for posts

--- hooks/usePosts.ts
+++ hooks/usePosts.ts
@@ -27,19 +27,18 @@ interface UsePosts {
     posts: Post[] | null;
     isFetching: boolean;
     error: string | null;
-    getPosts: (page?: number, limit?: number, skip?: number) => Promise<void>;
+    getPosts: (page?: number, limit?: number) => Promise<void>;
     pagination: PostResponse["pagination"] | null;
 }
 
-const usePosts = (): UsePosts => {
+const usePosts = (initialPage = 1): UsePosts => {
     const [posts, setPosts] = useState<Post[] | null>(null);
     const [isFetching, setIsFetching] = useState(true);
     const [error, setError] = useState<string | null>(null);
-    const [pagination, setPagination] = useState<
-        PostResponse["pagination"] | null
-    >(null);
+    const [pagination, setPagination] = useState<PostResponse["pagination"] | null>(null);
 
-    const getPosts = async (page = 1, limit = 8, skip = 0): Promise<void> => {
+    const getPosts = async (page = 1, limit = 8): Promise<void> => {
+        const skip = (page - 1) * limit;
         setIsFetching(true);
         setError(null);
         try {
@@ -60,8 +59,8 @@ const usePosts = (): UsePosts => {
     };
 
     useEffect(() => {
-        getPosts();
-    }, []);
+        getPosts(initialPage);
+    }, [initialPage]);
 
     return { posts, isFetching, error, getPosts, pagination };
 };
